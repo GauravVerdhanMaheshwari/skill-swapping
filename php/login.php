8acce646dbfa98a8988a66f1c347a7a518a51124
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-02-17 19:29:20 +0530

    completed login page

--- php/login.php
+++ php/login.php
@@ -11,15 +11,17 @@
 <body>
     <div class="login">
         <div class="innerLogin">
-            <form action="home.php" method="post">
+            <form action="" method="post">
                 <h1 class="heading">LOGIN</h1>
-                <label for="name">User Name</label><br>
+                <label for="userName">User Name</label><br>
                 <input type="text" class="input" name="userName" id="userName" autocomplete="off"
-                    placeholder="Enter your username" required minlength="5" maxlength="20"><br><br>
+                    placeholder="Enter your username" required minlength="5" maxlength="10"><br><br>
                 <label for="password">Password</label><br>
                 <input type="password" class="input" name="password" id="password" autocomplete="off"
-                    placeholder="Enter your username" required minlength="8" maxlength="20"><br><br>
-                <input type="submit" value="Login" class="button"> <br><br>
+                    placeholder="Enter your password" required minlength="8" maxlength="10"><br><br>
+                <p id="passwordWarning" class="warning"></p>
+
+                <input type="submit" name="Login" value="Login" class="button"> <br><br>
                 <div class="linkDiv">
                     <a href="index.php" class="link"> I don't have an account</a> <br>
                     <a href="forgetPassword.php" class="link"> Forget Password</a>
@@ -30,3 +32,55 @@
 </body>
 
 </html>
+
+<script>
+
+    document.getElementById("password").addEventListener("input", function () {
+        let passwordWarning = document.getElementById("passwordWarning");
+        if (this.value.length > 8) {
+            passwordWarning.innerText = "Password must be 8 characters long.";
+            passwordWarning.style.display = "block";
+        } else {
+            passwordWarning.style.display = "none";
+        }
+    });
+
+</script>
+
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "skill_swapping";
+
+// Create connection
+$connect = mysqli_connect($host, $username, $password, $db);
+
+// Check connection
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["Login"])) {
+    $name = mysqli_real_escape_string($connect, $_POST["userName"]);
+    $password = $_POST["password"]; // No need to escape, it's used in password_verify
+
+    // Query to fetch the hashed password from database
+    $checkName = "SELECT Password FROM user WHERE Name='$name'";
+    $result = mysqli_query($connect, $checkName);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $hash = $row['Password']; // Get stored hashed password
+
+        if (password_verify($password, $hash)) {
+            echo "<script>window.location.href='home.php';</script>";
+            exit;
+        } else {
+            echo "<script>alert('Incorrect Password!');</script>";
+        }
+    } else {
+        echo "<script>alert('Wrong username.');</script>";
+    }
+}
+?>
