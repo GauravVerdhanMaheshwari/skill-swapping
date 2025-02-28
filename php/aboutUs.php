7a70cfa4707630c288cf9c8c016742bc9bc426e9
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-03-01 00:25:38 +0530

    add some html and css

---
+++ php/aboutUs.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="../css/aboutUs.css">
+</head>
+
+<body>
+    <header class="head">
+        <a href="home.php" class="logo"><img src="../image/skillSwapping.png"></a>
+        <div class="innerHead">
+            <a href="courses.php">Courses</a>
+            <a href="aboutUs.php">About Us</a>
+        </div>
+        <a href="userProfile.php" class="profilePictureLink"><img src="../image/user.png" class="profilePicture"></a>
+    </header>
+    <div class="team">
+        <div class="person">
+            <a href="http://www.linkedin.com/in/gauravverdhanmaheshwari" class="link">
+                <img src="../image/Gaurav.jpeg" alt="Gaurav" class="image">
+            </a>
+            <h3 class="name">Gaurav Verdhan Maheswari</h3>
+        </div>
+        <div class="person">
+            <img src="../image/Gaurav.png" alt="Arya">
+            <h3 class="name">Arya</h3>
+        </div>
+        <div class="person">
+            <img src="../image/Gaurav.png" alt="Chintan">
+            <h3 class="name">Chintan</h3>
+        </div>
+        <div class="person">
+            <img src="../image/Gaurav.png" alt="Tarun">
+            <h3 class="name">Tarun</h3>
+        </div>
+        <div class="person">
+            <img src="../image/Gaurav.png" alt="Manthan">
+            <h3 class="name">Manthan</h3>
+        </div>
+        <div class="person">
+            <img src="../image/Gaurav.png" alt="Darsh">
+            <h3 class="name">Darsh</h3>
+        </div>
+    </div>
+
+</body>
+
+</html>
