6bb82e8d2ac89d601313cc19f1424f3736ec1193
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-02-28 17:13:13 +0530

    make some html of skill

---
+++ php/skillSelection.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skill Selection</title>
+    <link rel="stylesheet" href="../css/skillSelection.css">
+</head>
+
+<body>
+
+    <div class="wrap">
+        <input type="text" name="skill" id="skill" placeholder="Enter your skills">
+    </div>
+    <div class="skill">
+
+    </div>
+
+</body>
+
+<script>
+    let skills = document.getElementsByClassName('skill');
+</script>
+
+</html>
+
+<?php
+
+?>
