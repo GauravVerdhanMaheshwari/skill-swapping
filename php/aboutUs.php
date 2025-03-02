efbf33982bec1a15949d08a9deeef90e39f05031
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-03-02 14:19:23 +0530

    added 2 team members

--- php/aboutUs.php
+++ php/aboutUs.php
@@ -10,42 +10,49 @@
 
 <body>
     <header class="head">
-        <a href="home.php" class="logo"><img src="../image/skillSwapping.png"></a>
-        <div class="innerHead">
+        <a href="home.php" class="logo"><img src="../image/skillSwapping.png" alt="Logo"></a>
+        <nav class="innerHead">
             <a href="courses.php">Courses</a>
-            <a href="aboutUs.php">About Us</a>
-        </div>
-        <a href="userProfile.php" class="profilePictureLink"><img src="../image/user.png" class="profilePicture"></a>
+            <a href="aboutUs.php" class="active">About Us</a>
+        </nav>
+        <a href="userProfile.php" class="profilePictureLink">
+            <img src="../image/user.png" class="profilePicture" alt="User Profile">
+        </a>
     </header>
-    <div class="team">
-        <div class="person">
-            <a href="http://www.linkedin.com/in/gauravverdhanmaheshwari" class="link">
-                <img src="../image/Gaurav.jpeg" alt="Gaurav" class="image">
-            </a>
-            <h3 class="name">Gaurav Verdhan Maheswari</h3>
-        </div>
-        <div class="person">
-            <img src="../image/Gaurav.png" alt="Arya">
-            <h3 class="name">Arya</h3>
-        </div>
-        <div class="person">
-            <img src="../image/Gaurav.png" alt="Chintan">
-            <h3 class="name">Chintan</h3>
-        </div>
-        <div class="person">
-            <img src="../image/Gaurav.png" alt="Tarun">
-            <h3 class="name">Tarun</h3>
-        </div>
-        <div class="person">
-            <img src="../image/Gaurav.png" alt="Manthan">
-            <h3 class="name">Manthan</h3>
-        </div>
-        <div class="person">
-            <img src="../image/Gaurav.png" alt="Darsh">
-            <h3 class="name">Darsh</h3>
-        </div>
-    </div>
 
+    <main>
+        <h1 class="title">Meet Our Team</h1>
+        <div class="team">
+            <div class="person">
+                <a href="http://www.linkedin.com/in/gauravverdhanmaheshwari">
+                    <img width="225px" src="../image/Gaurav.jpeg" alt="Gaurav" class="image">
+                </a>
+                <h3 class="name">Gaurav Verdhan Maheshwari</h3>
+            </div>
+            <div class="person">
+                <a href="https://www.instagram.com/arya_makadia">
+                    <img width="200px" src="../image/Arya.jpeg" alt="Arya" class="image">
+                </a>
+                <h3 class="name">Makadia Arya</h3>
+            </div>
+            <div class="person">
+                <img width="140px" src="../image/Chintan.jpeg" alt="Chintan" class="image">
+                <h3 class="name">Malodiya Chintan Sanjaybhai</h3>
+            </div>
+            <div class="person">
+                <img src="../image/Tarun.jpeg" alt="Tarun" class="image">
+                <h3 class="name">Tarun</h3>
+            </div>
+            <div class="person">
+                <img src="../image/Manthan.jpeg" alt="Manthan" class="image">
+                <h3 class="name">Manthan</h3>
+            </div>
+            <div class="person">
+                <img src="../image/Darsh.jpeg" alt="Darsh" class="image">
+                <h3 class="name">Darsh</h3>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
