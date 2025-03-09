c9d74ed7cd425375bf8c66b0f5827076ef2183f8
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-03-09 14:19:45 +0530

    add user profile page with session validation and user data retrieval

---
+++ php/userProfile.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "skill_swapping";
+
+$connect = mysqli_connect($host, $username, $password, $db);
+
+session_start();
+if ($_SESSION["login"] == false) {
+    echo "<script>window.location.href='login.php';</script>";
+    exit;
+}
+
+$uid = $_SESSION["uid"];
+
+$query = "SELECT Name,Email FROM user WHERE UID = '$uid'";
+$result = mysqli_query($connect, $query);
+
+if ($query) {
+    $row = mysqli_fetch_assoc($result);
+    $name = $row["Name"];
+    $email = $row["Email"];
+}
+
+
+?>
